e22ae36a77170bd3065e53089860d7cbd6a15d65
aliibsamohammed <ibsamohammed@gmail.com>
2021-09-26 23:29:36 +0300

    static root

--- twitter_app/settings.py
+++ twitter_app/settings.py
@@ -82,6 +82,8 @@ BOOTSTRAP4 = { 'include_jquery': True }
 
 WSGI_APPLICATION = 'twitter_app.wsgi.application'
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
@@ -131,7 +133,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Activate Django-Heroku.
 django_heroku.settings(locals()) # <- this line should be a the end of the file
